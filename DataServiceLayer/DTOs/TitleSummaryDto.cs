def634d442b5b73c5e06b23c56244d53b3a88664
tamishl <hoogland.tn@gmail.com>
2025-12-12 14:52:59 +0100

    Add AllowAnyHeader and Method in WSL Program.

--- DataServiceLayer/DTOs/TitleSummaryDto.cs
+++ DataServiceLayer/DTOs/TitleSummaryDto.cs
@@ -5,7 +5,10 @@ public class TitleSummaryDto
     public string Id { get; set; }
     public string PrimaryTitle { get; set; }
     public string? StartYear { get; set; }
+    public string? EndYear { get; set; }
+    public int? RuntimeMinutes { get; set; }
     public string? Poster { get; set; }
+    public TitleRatingDto? TitleRating { get; set; }
     public string TypeId { get; set; }
  
 }
--- WebServiceLayer/Program.cs
+++ WebServiceLayer/Program.cs
@@ -29,7 +29,8 @@ builder.Services.AddCors(options =>
                       policy =>
                       {
                           policy.WithOrigins("http://localhost:5173")
-                          .AllowAnyHeader();
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
                       });
 
 });
